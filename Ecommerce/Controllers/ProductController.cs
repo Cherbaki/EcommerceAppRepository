d82039073f85ddf836f6dce3c255fadfb0d9faa4
Cherbaki <samudcherbak2018@gmail.com>
2023-03-13 23:04:59 +0400

    Add project files.

---
+++ Ecommerce/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+	public class ProductController : Controller
+	{
+
+
+		public IActionResult Index()
+		{
+			
+
+			return View();
+		}
+
+
+	}
+}
